8ed3097f027ba734cdd55ea3888b0ed8594372a1
Noman Shoukat <nomanaadma@gmail.com>
2020-11-10 18:15:52 +0500

    Basic Layout and Country Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Logo from "./components/Logo.jsx";
+import CountrySelect from "./components/CountrySelect.jsx";
+import { makeStyles, Container, Paper, Grid } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		padding: theme.spacing(2),
+		textAlign: "center",
+		color: theme.palette.text.secondary,
+	},
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const classes = useStyles();
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Logo />
+				<CountrySelect />
+			</header>
+			<Container maxWidth="xl">
+				<Grid container spacing={3}>
+					<Grid item xs={4}>
+						<Paper className={classes.paper}>xs=6</Paper>
+					</Grid>
+					<Grid item xs={8}>
+						<Paper className={classes.paper}>xs=6</Paper>
+					</Grid>
+					<Grid item xs={8}>
+						<Paper className={classes.paper}>xs=6</Paper>
+					</Grid>
+					<Grid item xs={4}>
+						<Paper className={classes.paper}>xs=6</Paper>
+					</Grid>
+				</Grid>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../logo.png";
+import "../App.css";
+
+function Logo() {
+	return (
+		<div className="alignCenter">
+			<img src={logo} alt="logo" />
+		</div>
+	)
+}
+
+export default Logo;
